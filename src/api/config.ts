90de1b48c915732726e914c086d738ffc7553a66
Yosip Mike Colin <colinparrado@gmail.com>
2025-04-14 17:59:03 -0500

    Change new URL

--- src/api/config.ts
+++ src/api/config.ts
@@ -12,7 +12,7 @@ export const axiosConfigBuddie = wrapper(
 );
 
 export const axiosConfig = axios.create({
-  baseURL: "https://we-sense-git-carriers-yosipmikecolins-projects.vercel.app/api",
+  baseURL: "https://wesense.netlify.app/api",
 });
 
 axiosConfig.interceptors.request.use(
