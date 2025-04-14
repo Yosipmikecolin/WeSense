53cb3bef4ba9496f5fc9f1c803d6a5375055faf0
Yosip Mike Colin <colinparrado@gmail.com>
2025-04-14 15:41:20 -0500

    Settings errors

--- src/views/view-requester/components/DetailsModal.tsx
+++ src/views/view-requester/components/DetailsModal.tsx
@@ -5,11 +5,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Requester } from "@/db/requester";
 import { RequestTable } from "@/views/view-create-request/interfaces";
 import { BriefcaseBusiness, Gavel, UserCheck } from "lucide-react";
 
 interface Props {
-  requester?: RequestTable;
+  requester?: Requester;
   open: boolean;
   onClose: VoidFunction;
 }
@@ -28,71 +29,71 @@ const DetailsModal = ({ requester, open, onClose }: Props) => {
         <div className="grid gap-4 mt-3">
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Nombre completo:</span>
-            <span>{requester?.requester.fullName}</span>
+            <span>{requester?.fullName}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Tipo:</span>
             <div className="w-28 flex items-center justify-between gap-2 bg-green-400 text-white py-1 px-2 font-bold rounded-md">
-              {requester?.requester.userType === "Abogado particular"
+              {requester?.userType === "Abogado particular"
                 ? "Abogado"
-                : requester?.requester.userType}
+                : requester?.userType}
 
-              {requester?.requester.userType === "Defensor" && <Gavel size={15} />}
+              {requester?.userType === "Defensor" && <Gavel size={15} />}
 
-              {requester?.requester.userType === "Abogado particular" && (
+              {requester?.userType === "Abogado particular" && (
                 <BriefcaseBusiness size={15} />
               )}
 
-              {requester?.requester.userType === "Otro" && <UserCheck size={15} />}
+              {requester?.userType === "Otro" && <UserCheck size={15} />}
             </div>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Telefono:</span>
-            <span className="flex gap-2">{requester?.requester.phone}</span>
+            <span className="flex gap-2">{requester?.phone}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Fecha de registro:</span>
-            <span>{requester?.requester.registrationDate}</span>
+            <span>{requester?.registrationDate}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Institución:</span>
-            <span>{requester?.requester.institution}</span>
+            <span>{requester?.institution}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Télefono:</span>
-            <span>{requester?.requester.phone}</span>
+            <span>{requester?.phone}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Número de identificación:</span>
-            <span>{requester?.requester.identificationNumber}</span>
+            <span>{requester?.identificationNumber}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">RUC:</span>
-            <span>{requester?.requester.ruc}</span>
+            <span>{requester?.ruc}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Ciudad:</span>
-            <span>{requester?.requester.region}</span>
+            <span>{requester?.region}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Dirección:</span>
-            <span>{requester?.requester.address}</span>
+            <span>{requester?.address}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Áreas de acceso:</span>
-            <span>{requester?.requester.accessAreas}</span>
+            <span>{requester?.accessAreas}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Verificación de identidad:</span>
-            <span>{requester?.requester.identityVerification}</span>
+            <span>{requester?.identityVerification}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Pregunta de seguridad:</span>
-            <span>{requester?.requester.securityQuestion}</span>
+            <span>{requester?.securityQuestion}</span>
           </div>
           <div className="grid grid-cols-2 items-center gap-4">
             <span className="font-semibold">Observaciones:</span>
-            <span>{requester?.requester.observations}</span>
+            <span>{requester?.observations}</span>
           </div>
         </div>
       </DialogContent>
