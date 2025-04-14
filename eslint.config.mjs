b54ed505cbd58ce2df30cd94aa16326301c0270c
Yosip Mike Colin <colinparrado@gmail.com>
2025-04-14 15:15:09 -0500

    Settings 4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,21 +13,16 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Si tienes un tipo `any`
+      "@typescript-eslint/ban-types": "off", // Si estás usando un tipo no permitido
+      "@typescript-eslint/no-inferrable-types": "off", // Para deshabilitar tipos inferribles
+      "@typescript-eslint/no-unused-vars": "off", // Para deshabilitar advertencias de variable
       "no-var": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn", // O "error" si prefieres advertencias
         {
           argsIgnorePattern: "^_", // Permite variables que comienzan con "_"
           varsIgnorePattern: "^_", // Permite variables que comienzan con "_"
-        },
-      ],
-      "@typescript-eslint/ban-types": [
-        "warn",
-        {
-          types: {
-            "{}": false, // Permite usar "{}" o context sin tipo explícito
-          },
-          extendDefaults: true,
         },
       ],
     },
