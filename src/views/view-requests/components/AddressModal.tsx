04d1315bac3cbcf2a0d9fd59a85baaaa2a61c2a5
Yosip Mike Colin <colinparrado@gmail.com>
2025-04-18 14:06:34 -0500

    Add getHour

--- src/views/view-requests/components/AddressModal.tsx
+++ src/views/view-requests/components/AddressModal.tsx
@@ -76,13 +76,22 @@ const AddressModal = ({
     setMinimumCoverage("");
   };
 
+  function getHour(): string {
+    const ahora = new Date();
+    const horas = ahora.getHours().toString().padStart(2, "0");
+    const minutos = ahora.getMinutes().toString().padStart(2, "0");
+    const segundos = ahora.getSeconds().toString().padStart(2, "0");
+
+    return `${horas}:${minutos}:${segundos}`;
+  }
+
   const handleSubmit = async () => {
     if (request) {
       setIsLoading(true);
       try {
         await updatedRequest({
           ...request,
-          response_date: getDate(),
+          response_date: getDate() + getHour(),
           time_respond: "",
           answer: status,
           status: "answered",
